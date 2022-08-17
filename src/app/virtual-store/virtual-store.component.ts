bb6cb5175016b7744abffdf1a491b120ae3f7403
Jhon Gonzalez <jhonja971106@gmail.com>
2022-08-17 10:47:06 -0500

    Cambios parciales para virtual store

--- src/app/virtual-store/virtual-store.component.ts
+++ src/app/virtual-store/virtual-store.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Viewer } from 'photo-sphere-viewer/src/Viewer'
 
 @Component({
   selector: 'app-virtual-store',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./virtual-store.component.scss']
 })
 export class VirtualStoreComponent implements OnInit {
-
+  
+  viewer : Viewer;
   constructor() { }
 
   ngOnInit(): void {
+    this.viewer = new Viewer({
+      container: document.querySelector('#viewer') as HTMLCanvasElement ,
+      panorama: 'https://photo-sphere-viewer-data.netlify.app/assets/sphere.jpg',
+      defaultLat: -Math.PI / 2,
+      defaultLong: Math.PI,
+      defaultZoomLvl: 0,
+      fisheye: 4,
+      navbar: [
+        'zoom',
+        {
+          content: 'Rerun&nbsp;animation',
+          onClick: this.intro,
+        },
+        'caption',
+        'fullscreen'
+      ]
+    });
+    this.intro();
   }
 
+  intro(){
+    debugger
+    // default far plane is too close to render fisheye=4
+    // you can also skip this line and start with fisheye=2
+    // this.viewer.renderer.camera.far *= 2;
+
+    // new Viewer.utils.Animation({
+    //   properties: {
+    //     lat: { start: -Math.PI / 2, end: 0.2 },
+    //     long: { start: Math.PI, end: 0 },
+    //     zoom: { start: 0, end: 50 },
+    //     fisheye: { start: 4, end: 0 },
+    //   },
+    //   duration: 2000,
+    //   easing: 'inOutQuad',
+    //   onTick: (properties) => {
+    //     this.viewer.setOption('fisheye', properties.fisheye);
+    //     this.viewer.rotate({ longitude: properties.long, latitude: properties.lat });
+    //     this.viewer.zoom(properties.zoom);
+    //   }
+    // });
+  }
 }
