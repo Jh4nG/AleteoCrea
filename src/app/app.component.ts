79c9b78e566c8069a158e5fca88b5fc04489ec2f
Jhon Gonzalez <jhonja971106@gmail.com>
2022-08-30 20:42:02 -0500

    Boton flotante extra modos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,98 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  flag=0;
   constructor(private spinner: NgxSpinnerService) {}
+
   ngOnInit(): void {
     this.spinner.show();
+    setTimeout(()=>{
+      this.spinner.hide();
+    },8500);
   }
 
+  
+  shareFunction(){
+    const left = 96;
+    const position = 90;
+    let one = document.querySelector('.one') as HTMLElement;
+    let two = document.querySelector('.two') as HTMLElement;
+    let three = document.querySelector('.three') as HTMLElement;
+    let four = document.querySelector('.four') as HTMLElement;
+    if (this.flag == 0) {
+      one.animate({
+        top: `${position-10}%`,
+        left: `${left+2}%`,
+      }, 200);
+      setTimeout(() => {
+        one.style.top = `${position-10}%`;
+        one.style.left = `${left+2}%`;
+      },210);
+
+      setTimeout(() => {
+        two.animate({
+          top: `${position-9}%`,
+          left: `${left-2}%`
+        }, 200);
+        setTimeout(() => {
+          two.style.top = `${position-9}%`;
+          two.style.left = `${left-2}%`;
+        },210);
+      },200);
+
+      setTimeout(() => {
+        three.animate({
+          top: `${position-3}%`,
+          left: `${left-5}%`
+        }, 200);
+        setTimeout(() => {
+          three.style.top = `${position-3}%`;
+          three.style.left = `${left-5}%`;
+        },210);
+      },300);
+
+      setTimeout(() => {
+        four.animate({
+          top: `${position+6}%`,
+          left: `${left-5}%`
+        }, 200);
+        setTimeout(() => {
+          four.style.top = `${position+6}%`;
+          four.style.left = `${left-5}%`;
+        },210);
+      },400);
+      this.flag = 1;
+    
+    } else {
+      one.animate({
+        top: `${position}%`,
+        left: `${left}%`
+      }, 200);
+      two.animate({
+        top: `${position}%`,
+        left: `${left}%`
+      }, 200);
+      three.animate({
+        top: `${position}%`,
+        left: `${left}%`
+      }, 200);
+      four.animate({
+        top: `${position}%`,
+        left: `${left}%`
+      }, 200);
+
+      setTimeout(() => {
+        one.style.top = `${position}%`;
+        one.style.left = `${left}%`;
+        two.style.top = `${position}%`;
+        two.style.left = `${left}%`;
+        three.style.top = `${position}%`;
+        three.style.left = `${left}%`;
+        four.style.top = `${position}%`;
+        four.style.left = `${left}%`;
+      },205);
+      this.flag = 0;
+    }
+  }
   
 }
