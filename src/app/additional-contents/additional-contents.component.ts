823baee118e23634cd9af03b1f539f93a420818b
JhonG <jhonja971106@gmail.com>
2022-11-13 07:55:31 -0500

    Cambios contenidos adicionales y git ignore

--- src/app/additional-contents/additional-contents.component.ts
+++ src/app/additional-contents/additional-contents.component.ts
@@ -1,19 +1,63 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit, AfterContentInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+declare var $;
 @Component({
   selector: 'app-additional-contents',
   templateUrl: './additional-contents.component.html',
   styleUrls: ['./additional-contents.component.scss']
 })
-export class AdditionalContentsComponent implements OnInit, OnDestroy {
+export class AdditionalContentsComponent implements OnInit, OnDestroy, AfterContentInit {
 
-  constructor() { }
+  videoActualSrc : any = "";
+  spinerMariposa = "MariposaSpinner";
+  constructor(private spinner: NgxSpinnerService) { }
 
   ngOnDestroy(): void {
     window.location.reload();
   }
 
+  ngAfterContentInit(): void {
+    // this.spinner.show(this.spinerMariposa);
+    // setTimeout(()=>{
+    //   this.spinner.hide(this.spinerMariposa);
+    // },4000);
+  }
+
   ngOnInit(): void {
+  }
+
+  openViewContent(content){
+    let video = document.getElementById('videoContentAdd') as HTMLVideoElement;
+    let name = document.getElementById('nameVideo') as HTMLElement;
+    switch(content){
+      case 'aleteo':
+        this.videoActualSrc = "../../assets/img/contentsAdd/Videos/¿Qué es Aleteo-.mp4";
+        // name.innerHTML = "aleteo";
+        break;
+      case 'bia':
+        this.videoActualSrc = "../../assets/img/contentsAdd/Videos/BIA.mp4";
+        // name.innerHTML = "bia";
+        break;
+      case 'camila':
+        this.videoActualSrc = "../../assets/img/contentsAdd/Videos/Camila.mp4";
+        // name.innerHTML = "camila";
+        break;
+      case 'museo':
+        this.videoActualSrc = "../../assets/img/contentsAdd/Videos/Museo Interactivo.mp4";
+        // name.innerHTML = "museo interavtivo";
+        break;
+    }
+    video.src = this.videoActualSrc;
+    video.play();
+    video.volume = 0.7;
+    $('#modalContentAdd').modal('show');
+  }
+
+  eventCloseModalContentAdd(){
+    let video = document.getElementById('videoContentAdd') as HTMLVideoElement;
+    video.pause();
+    video.currentTime = 0;
+    video.src = "";
   }
 
 }
