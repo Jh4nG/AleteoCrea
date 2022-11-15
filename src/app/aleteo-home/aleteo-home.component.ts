398fbb943cd998c2c5f25f3a80aa960c9444dfc7
JefersonPineros <jeferson.pineros@gmail.com>
2022-11-14 23:06:22 -0500

    fix: correcciones arbol y home

--- src/app/aleteo-home/aleteo-home.component.ts
+++ src/app/aleteo-home/aleteo-home.component.ts
@@ -37,81 +37,81 @@ export class AleteoHomeComponent implements OnInit, OnDestroy, AfterViewInit {
   
   ngAfterViewInit(): void {
     // animación podcast
-    let podcastOver = document.getElementById('podcast-link');
-    podcastOver.addEventListener('mouseover', () => {
-      let podcasText = document.getElementById('podcast-animated');
-      podcasText.classList.forEach((item) => {
-        if(item == 'animate__fadeOut') {
-          podcasText.classList.remove('animate__fadeOut');
-        }
-        if(item == 'show-item') {
-          podcasText.classList.remove('show-item');
-        }
-      });
-      podcasText.classList.add('animate__fadeIn');
-      setTimeout(()=> {
-        podcasText.classList.remove('animate__fadeIn');
-        podcasText.classList.add('animate__fadeOut');
-      }, 1500);
-    });
-    // animación Tienda
-    let tiendaOver = document.getElementById('tienda-link');
-    tiendaOver.addEventListener('mouseover', () => {
-      let tiendaText = document.getElementById('tienda-animated');
-      tiendaText.classList.forEach((item) => {
-        if(item == 'animate__fadeOut') {
-          tiendaText.classList.remove('animate__fadeOut');
-        }
-        if(item == 'show-item') {
-          tiendaText.classList.remove('show-item');
-        }
-      });
-      tiendaText.classList.add('animate__fadeIn');
-      setTimeout(()=> {
-        tiendaText.classList.remove('animate__fadeIn');
-        tiendaText.classList.add('animate__fadeOut');
-      }, 1500);      
-    });
+    // let podcastOver = document.getElementById('podcast-link');
+    // podcastOver.addEventListener('mouseover', () => {
+    //   let podcasText = document.getElementById('podcast-animated');
+    //   podcasText.classList.forEach((item) => {
+    //     if(item == 'animate__fadeOut') {
+    //       podcasText.classList.remove('animate__fadeOut');
+    //     }
+    //     if(item == 'show-item') {
+    //       podcasText.classList.remove('show-item');
+    //     }
+    //   });
+    //   podcasText.classList.add('animate__fadeIn');
+    //   setTimeout(()=> {
+    //     podcasText.classList.remove('animate__fadeIn');
+    //     podcasText.classList.add('animate__fadeOut');
+    //   }, 1500);
+    // });
+    // // animación Tienda
+    // let tiendaOver = document.getElementById('tienda-link');
+    // tiendaOver.addEventListener('mouseover', () => {
+    //   let tiendaText = document.getElementById('tienda-animated');
+    //   tiendaText.classList.forEach((item) => {
+    //     if(item == 'animate__fadeOut') {
+    //       tiendaText.classList.remove('animate__fadeOut');
+    //     }
+    //     if(item == 'show-item') {
+    //       tiendaText.classList.remove('show-item');
+    //     }
+    //   });
+    //   tiendaText.classList.add('animate__fadeIn');
+    //   setTimeout(()=> {
+    //     tiendaText.classList.remove('animate__fadeIn');
+    //     tiendaText.classList.add('animate__fadeOut');
+    //   }, 1500);      
+    // });
 
-    // animación Arbol
-    let ArbolOver = document.getElementById('arbol-link');
-    ArbolOver.addEventListener('mouseover', () => {
-      let arbolText = document.getElementById('arbol-animated');
-      arbolText.classList.forEach((item) => {
-        if(item == 'animate__fadeOut') {
-          arbolText.classList.remove('animate__fadeOut');
-        }
-        if(item == 'show-item') {
-          arbolText.classList.remove('show-item');
-        }
-      });
-      arbolText.classList.add('animate__fadeIn');
-      setTimeout(()=> {
-        arbolText.classList.remove('animate__fadeIn');
-        arbolText.classList.add('animate__fadeOut');
-      }, 1500); 
+    // // animación Arbol
+    // let ArbolOver = document.getElementById('arbol-link');
+    // ArbolOver.addEventListener('mouseover', () => {
+    //   let arbolText = document.getElementById('arbol-animated');
+    //   arbolText.classList.forEach((item) => {
+    //     if(item == 'animate__fadeOut') {
+    //       arbolText.classList.remove('animate__fadeOut');
+    //     }
+    //     if(item == 'show-item') {
+    //       arbolText.classList.remove('show-item');
+    //     }
+    //   });
+    //   arbolText.classList.add('animate__fadeIn');
+    //   setTimeout(()=> {
+    //     arbolText.classList.remove('animate__fadeIn');
+    //     arbolText.classList.add('animate__fadeOut');
+    //   }, 1500); 
       
-    });
+    // });
 
-    // animación contenidos
-    let contenidosOver = document.getElementById('contenidos-link');
-    contenidosOver.addEventListener('mouseover', () => {
-      let contenidoText = document.getElementById('contenidos-animated');
-      contenidoText.classList.forEach((item) => {
-        if(item == 'animate__fadeOut') {
-          contenidoText.classList.remove('animate__fadeOut');
-        }
-        if(item == 'show-item') {
-          contenidoText.classList.remove('show-item');
-        }
-      });
-      contenidoText.classList.add('animate__fadeIn');
-      setTimeout(()=> {
-        contenidoText.classList.remove('animate__fadeIn');
-        contenidoText.classList.add('animate__fadeOut');
-      }, 1500); 
+    // // animación contenidos
+    // let contenidosOver = document.getElementById('contenidos-link');
+    // contenidosOver.addEventListener('mouseover', () => {
+    //   let contenidoText = document.getElementById('contenidos-animated');
+    //   contenidoText.classList.forEach((item) => {
+    //     if(item == 'animate__fadeOut') {
+    //       contenidoText.classList.remove('animate__fadeOut');
+    //     }
+    //     if(item == 'show-item') {
+    //       contenidoText.classList.remove('show-item');
+    //     }
+    //   });
+    //   contenidoText.classList.add('animate__fadeIn');
+    //   setTimeout(()=> {
+    //     contenidoText.classList.remove('animate__fadeIn');
+    //     contenidoText.classList.add('animate__fadeOut');
+    //   }, 1500); 
       
-    });
+    // });
     
   }
   routePages(page: string): void {
